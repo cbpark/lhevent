dea8dc358183d73fe196486253ada50e15121bf0
cbpark <cbpark@gmail.com>
2017-12-13 22:53:00 +0900

    Update nix and stack files

--- shell.nix
+++ shell.nix
@@ -1,33 +1,16 @@
-{ nixpkgs ? import <nixpkgs> {}, compiler ? "default" }:
+{ nixpkgs ? import <nixpkgs> {}, compiler ? "default", doBenchmark ? false }:
 
 let
 
   inherit (nixpkgs) pkgs;
 
-  f = { mkDerivation, attoparsec, base, bytestring, containers
-      , hep-utilities, pipes, pipes-attoparsec, pipes-bytestring, stdenv
-      , transformers
-      }:
-      mkDerivation {
-        pname = "lhevent";
-        version = "0.0.0.0";
-        src = ./.;
-        isLibrary = true;
-        isExecutable = false;
-        libraryHaskellDepends = [
-          attoparsec base bytestring containers hep-utilities pipes
-          pipes-attoparsec pipes-bytestring transformers
-        ];
-        homepage = "https://github.com/cbpark/lhevent";
-        description = "Tools for analyzing the Monte Carlo event data in high energy physics";
-        license = stdenv.lib.licenses.bsd3;
-      };
-
   haskellPackages = if compiler == "default"
                        then pkgs.haskellPackages
                        else pkgs.haskell.packages.${compiler};
 
-  drv = haskellPackages.callPackage f {};
+  variant = if doBenchmark then pkgs.haskell.lib.doBenchmark else pkgs.lib.id;
+
+  drv = variant (haskellPackages.callPackage ./default.nix {});
 
 in
 
