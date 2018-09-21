a2d977494e4e62939108e637df6c168186790591
cbpark <cbpark@gmail.com>
2018-09-21 20:24:49 +0900

    Add build.nix, remove other files for building

---
+++ build.nix
@@ -0,0 +1,12 @@
+{ nixpkgs ? import <nixpkgs> {}, compiler ? "default" }:
+
+let
+  inherit (nixpkgs) pkgs;
+
+  haskellPackages = if compiler == "default"
+                      then pkgs.haskellPackages
+                      else pkgs.haskell.packages.${compiler};
+
+in
+  { lhevent = haskellPackages.callCabal2nix "lhevent" ./. {};
+  }
